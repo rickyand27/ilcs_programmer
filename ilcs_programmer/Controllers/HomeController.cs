5501ad851ca17e57d023a13842ede9e9b9499664
Ricky Andriansyah <idrickyandriansyah@gmail.com>
2023-12-04 17:43:39 +0700

    [add]initialize project

---
+++ ilcs_programmer/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ilcs_programmer.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class ILCSController : ControllerBase
+    {
+        [HttpGet("Form")]
+        public IActionResult GetExample(
+            [FromQuery] string Negara,
+            [FromQuery] string Pelabuhan,
+            [FromQuery] string Barang,
+            [FromQuery] double Harga,
+            [FromQuery] double TarifBeaMasuk)
+        {
+            var totalBiaya = TarifBeaMasuk / 100 * Harga;
+
+            var result = new List<string>
+            {
+                $"Negara : {Negara}",
+                $"Pelabuhan : {Pelabuhan}",
+                $"Barang : {Barang}",
+                $"Detail Barang {Barang} berupa seperti yang disebutkan",
+                $"Harga : {Harga}",
+                $"Tarif Bea Masuk : {TarifBeaMasuk}",
+                $"Total Biaya : {totalBiaya}"
+
+            };
+            return Ok(result);
+        }
+    }
+
+}
